aa0c536c9b0ac06f595e19001b75797f044637bd
masao.setogawa <ms.job.hunting@gmail.com>
2021-11-17 23:28:32 +0900

    Add original image setting to face

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/weights")));
 app.use(express.static(path.join(__dirname, "/js")));
+app.use(express.static(path.join(__dirname, "/images")));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/faceDetection.html"))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+app.listen(3000, () =>
+  console.log("Listening on port 3000! http://localhost:3000")
 );
-app.listen(3000, () => console.log("Listening on port 3000!"));
